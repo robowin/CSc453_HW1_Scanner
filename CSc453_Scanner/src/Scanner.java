8fd723e2eeabc94591759b9bbfee89c464cd60f0
zongtaofu <859982246@qq.com>
2019-09-16 22:49:39 -0700

    V1

--- CSc453_Scanner/src/Scanner.java
+++ CSc453_Scanner/src/Scanner.java
@@ -1,6 +1,6 @@
 public class Scanner{
   enum TokenType{
-      NUM, PLUS, MINUS, MUL, DIV, LT, LTE, GT, GTE, aa;
+      NUM, PLUS, MINUS, MUL, DIV, LT, LTE, GT, GTE;
   }
 
   class Token{
@@ -16,23 +16,46 @@ public class Scanner{
   }
 
   public Token extractToken(StringBuilder stream){
-    /* TODO #2: Extract the next token in the string, or report an error*/
-    return null;
+	  String arg = stream.toString();
+	  char firstChar = arg.charAt(0);
+	  String firstStringChar = arg.substring(0, 1);
+	  Token firstToken = null;
+	  
+	  if (firstChar == '>') { 
+		  firstToken = new Token(TokenType.GT, firstStringChar);
+		  char secondChar = arg.charAt(1);
+		  if (secondChar == '=') {
+			  String firstTwoStringChar = arg.substring(0,2);
+			  firstToken = new Token(TokenType.GTE, firstTwoStringChar);
+		  }
+	  } else if (firstChar == '<') {
+		  firstToken = new Token(TokenType.LT, firstStringChar);
+		  char secondChar = arg.charAt(1);
+		  if (secondChar == '=') {
+			  String firstTwoStringChar = arg.substring(0,2);
+			  firstToken = new Token(TokenType.LTE, firstTwoStringChar);
+		  }
+	  } else if (Character.isDigit(firstChar)) {
+		  firstToken = new Token(TokenType.NUM, firstStringChar);
+	  } else if (firstChar == '+') {
+		  firstToken = new Token(TokenType.PLUS, firstStringChar);
+	  } else if (firstChar == '-') {
+		  firstToken = new Token(TokenType.MINUS, firstStringChar);
+	  } else if (firstChar == '*') {
+		  firstToken = new Token(TokenType.MUL, firstStringChar);
+	  } else if (firstChar == '/') {
+		  firstToken = new Token(TokenType.DIV, firstStringChar);
+	  }
+	 return firstToken;
   }
-  
 
-  public String extractTokens(String arg){
-    /* TODO #1: Finish this function to iterate over all tokens in the input string.
-
-       Pseudo code:
-       String extractTokens(String arg):
-         String result= â€œâ€�;
-         while(arg is not empty)
-            Token nextToken = extractToken(arg)
-            result += nextToken.toString()
-         return result
-    */
-    return null;
+public String extractTokens(String arg){
+	  String result = "";
+	  while (!arg.isEmpty()) {
+		  Token nextToken = extractToken(new StringBuilder(arg));
+		  result += nextToken.toString();
+	  }
+    return result;
   }
 
 }
